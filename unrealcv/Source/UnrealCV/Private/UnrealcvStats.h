df52ba060247f1829e47db39408373e4c494b4fe
Jiawei Ren <rjw20020718@163.com>
2025-04-27 18:17:25 -0700

    fix: change unrealcv

--- unrealcv/Source/UnrealCV/Private/UnrealcvStats.h
+++
@@ -1,4 +0,0 @@
-// Weichao Qiu @ 2018
-#pragma once
-#include "Runtime/Core/Public/Stats/Stats.h"
-DECLARE_STATS_GROUP(TEXT("UnrealCV"), STATGROUP_UnrealCV, STATCAT_Advanced);
--- unrealcv/examples/WIP/arm.py
+++
@@ -1,9 +0,0 @@
-# A virtual robot arm demo to show blueprint interop, domain-adaptation and collision event
-from unrealcv import client
-
-def main():
-    client.connect()
-    client.request('vexec ArmController_C_0 ToggleRandomPose')
-
-if __name__ == '__main__':
-    main()
